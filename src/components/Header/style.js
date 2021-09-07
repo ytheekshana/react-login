1359dfc38af6046d0720e9e6da7642a273eb4001
Yasiru Nayanajith <ytheekshana@yahoo.com>
2021-09-07 21:32:57 +0530

    Initial Push

---
+++ src/components/Header/style.js
@@ -0,0 +1,33 @@
+
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignItems:"left",
+  },
+  appbar:{
+    backgroundColor:theme.palette.info.main,
+  },
+  logo:{
+    width:45,
+    height:45,
+    marginRight:"15px",
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+
+  },
+  button:{
+    color:"#91f299",
+  }
+
+}));
